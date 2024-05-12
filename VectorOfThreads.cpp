92d4f9496a36ddc43d93bfee8b274e1aa0f45482
yk68-github <yannik.laurent@outlook.com>
2024-05-12 07:17:15 +0200

    std::function<void(const unsigned int)> func = [](const unsigned int pause)

--- VectorOfThreads.cpp
+++ VectorOfThreads.cpp
@@ -6,31 +6,21 @@
 
 int main()
 {
-    // Create a vector of threads
+    const auto processor_count = std::thread::hardware_concurrency();
     std::vector<std::thread> vecOfThreads;
-    // Create a function object
-    std::function<void()> func = []() {
-        //Do Some Important Work
-        // .....
-        //Print Thread ID
+
+    std::function<void(const unsigned int)> func = [](const unsigned int pause) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(pause));
         std::cout << "From Thread ID : " << std::this_thread::get_id() << "\n";
         };
-    // Add a Thread object to vector
-    vecOfThreads.push_back(std::thread(func));
-    // Create 3 differet thread objects
-    std::thread th1(func);
-    std::thread th2(func);
-    std::thread th3(func);
-    // Move all three thread objects to vector
-    vecOfThreads.push_back(std::move(th1));
-    vecOfThreads.push_back(std::move(th2));
-    vecOfThreads.push_back(std::move(th3));
-    // Do some important work in main thread.
-    /** Wait for all the threads in vector to join **/
-    // Iterate over the thread vector
+
+    for (unsigned int i = 0; i < (processor_count - 2); i++)
+    {
+         vecOfThreads.push_back((std::thread(func, 100)));
+    }
+
     for (std::thread& th : vecOfThreads)
     {
-        // If thread Object is Joinable then Join that thread.
         if (th.joinable())
             th.join();
     }
